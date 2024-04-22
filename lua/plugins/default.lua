6db5b6d674d60e04557b918ff6f08356d1109e4b
johnjaller <johnjaller@hotmail.com>
2024-04-22 18:30:00 +0800

    feat: autotag option

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -115,6 +115,12 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+      autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+      },
       ensure_installed = {
         "bash",
         "html",
